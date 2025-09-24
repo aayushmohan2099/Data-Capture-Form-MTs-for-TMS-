ba501185398ec4a4a6c325d5f7a8eda0ce7c6eb0
AyushM@BDO <ayushmohan193@gmail.com>
2025-09-24 15:27:53 +0530

    Added File Size constraints for Certificates | Battling error code 403

--- dcmaster/settings.py
+++ dcmaster/settings.py
@@ -154,6 +154,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Media MAX size (50 mb limit)
+DATA_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024  
+FILE_UPLOAD_MAX_MEMORY_SIZE = 50 * 1024 * 1024  
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
